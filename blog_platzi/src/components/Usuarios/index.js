a5859723c043c244c87864fed590892b8dc9e181
David Almeida <djalmeida3@gmail.com>
2022-08-02 16:25:13 -0400

    Adding extra functionality

---
+++ blog_platzi/src/components/Usuarios/index.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux'
+import Spinner from "../General/Spinner";
+import Fatal from "../General/Fatal";
+
+import * as usuariosActions from '../../actions/usuariosActions'
+import "../../css/index.css";
+
+class Usuarios extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: []
+    }
+  }
+
+  componentDidMount() {
+    this.props.traerTodos();
+  }
+
+  ponerContenido = () => {
+    if (this.props.cargando) {
+      return <Spinner/>
+    }
+    if (this.props.error) {
+      return <Fatal mensaje={this.props.error}/>
+    }
+    return (
+      <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correos</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.ponerFilas() }
+          </tbody>
+        </table>
+    );
+  }
+
+  ponerFilas = () => (
+    this.props.usuarios.map((usuario) => (
+      <tr key={ usuario.id }>
+        <td>
+          { usuario.name }
+        </td>
+        <td>
+          { usuario.email }
+        </td>
+        <td>
+          { usuario.website }
+        </td>
+      </tr>
+    ))
+  )
+
+  render() {
+    return(
+      <div>
+        {this.ponerContenido()}
+      </div>
+    );
+  };
+}
+
+const mapStateToProps = (reducers) => {
+  return reducers.usuariosReducer;
+};
+
+export default connect(mapStateToProps, usuariosActions)(Usuarios);
--- blog_platzi/src/index.js
+++ blog_platzi/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+const store = createStore(
+	reducers, // Reducers
+	{}, // Estado inicial
+	applyMiddleware(reduxThunk)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+	<Provider store={ store }>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
